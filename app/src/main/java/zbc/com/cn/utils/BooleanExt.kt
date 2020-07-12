3fc8d48c8ee8b39ffea41fce0f73b23c4d6800e1
benchengzhou <1107472286@qq.com>
2020-07-12 23:41:26 +0800

    A

---
+++ app/src/main/java/zbc/com/cn/utils/BooleanExt.kt
@@ -0,0 +1,50 @@
+package zbc.com.cn.utils
+
+import kotlin.contracts.Returns
+
+
+sealed class BooleanExt<out T> {}
+
+object OtherWise : BooleanExt<Nothing>()
+
+class withData<T>(val data: T) : BooleanExt<T>()
+
+fun <T> BooleanExt<T>.otherwise(block: () -> T): T {
+    when (this) {
+        is OtherWise -> {
+            return block()
+        }
+        is withData -> {
+            return this.data
+        }
+    }
+}
+
+
+//https://blog.csdn.net/Jaden_hool/article/details/78437947
+inline fun <T> Boolean.yes(block: () -> T): BooleanExt<T> {
+    when {
+        this -> {
+            return withData(block())
+        }
+        else -> {
+            return OtherWise
+        }
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val lambda = {
+        print("OK")
+        1
+    }
+
+    val lambda2 = {
+        print("NOT OK !")
+        2
+    }
+    val a = true.yes(lambda).otherwise(lambda2)
+    print(a)
+}
+
