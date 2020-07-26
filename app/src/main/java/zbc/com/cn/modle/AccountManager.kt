b2f12bf17265686bf6c5132102e7caeed52e8ea6
benchengzhou <1107472286@qq.com>
2020-07-27 00:12:23 +0800

    代码完善

--- app/src/main/java/zbc/com/cn/modle/AccountManager.kt
+++ app/src/main/java/zbc/com/cn/modle/AccountManager.kt
@@ -1,6 +1,7 @@
 package zbc.com.cn.modle
 
 object AccountManager {
+    var authId by pref(-1)
     var userName by pref("")
     var password by pref("")
     var token by pref("")
--- app/src/main/java/zbc/com/cn/utils/LogExt.kt
+++ app/src/main/java/zbc/com/cn/utils/LogExt.kt
@@ -1,11 +1,11 @@
 package zbc.com.cn.utils
 
+import android.os.Build
+import android.preference.Preference
+import androidx.annotation.RequiresApi
 import com.orhanobut.logger.Logger
+import zbc.com.cn.application.AppContext
 
 
-//val loggerMap = HashMap<Class<*>, Logger>()
-//
-//inline val <reified T>T.logger:Logger
-//    get()=
-//        return loggerMap[T::class.java]?
-
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+inline fun <reified R, T> R.pref(default: T) = Preference(AppContext, null, 0, R::class.jvmName)
